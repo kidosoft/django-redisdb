dbc5a96e7ada37bdba6a608476722ecc8566681d
jstolarski <none@none>
2015-06-30 21:24:43 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
 
 setup(
     name='django-redisdb',
-    version='0.1.0',
+    version='0.2.0',
     description='Django redis backend',
     long_description=long_description,
     url='http://django-redisdb.readthedocs.org',
